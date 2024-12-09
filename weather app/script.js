496ea7ac5dc1b8b160132b01888f15991152fffb
Abir panda <abirpanda335@gmail.com>
2024-12-09 13:30:39 +0530

    added weather images for background

--- weather app/script.js
+++ weather app/script.js
@@ -1,6 +1,6 @@
 function getweather(){
     var city= document.getElementById("city").value;
-    var apikey='apikey'
+    var apikey='a39c1429152507e775150783a7006a0c'
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`;
     
@@ -33,6 +33,7 @@ function displayweather(data){
     const cloudy=document.querySelector('#cloudy')
     const humidity=document.querySelector('#humidity')
     const winds=document.querySelector('#winds')
+    const body=document.body
 
     weatherinfo.innerHTML=''
     temp.innerHTML=''
@@ -63,6 +64,51 @@ function displayweather(data){
         cloudy.innerText=`${clouds}%`
         humidity.innerHTML=`${humiditydata}%`
         weathericon.src=iconurl
+
+
+        const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+        const sunrise = data.sys.sunrise; // Sunrise time in seconds
+        const sunset = data.sys.sunset; // Sunset time in seconds
+
+        let backgroundImage= '';
+
+        if (currentTime >= sunrise && currentTime < sunset) {
+
+        if (description.includes('clear')) {
+            backgroundImage = "url('https://images.unsplash.com/photo-1476673160081-cf065607f449?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+        } else if (description.includes('clouds')) {
+            backgroundImage = "url('https://images.pexels.com/photos/158163/clouds-cloudporn-weather-lookup-158163.jpeg')";
+        } else if (description.includes('rain')) {
+            backgroundImage = "url('https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+        } else if (description.includes('snow')) {
+            backgroundImage = "url('https://images.unsplash.com/photo-1483664852095-d6cc6870702d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+        } else if (description.includes('thunderstorm')) {
+            backgroundImage = "url('https://images.pexels.com/photos/3657143/pexels-photo-3657143.jpeg?auto=compress&cs=tinysrgb&w=600')";
+        } else {
+            backgroundImage = "url('https://img.freepik.com/free-photo/colorful-sky-after-sunset_661209-461.jpg')"; // Fallback image
+        }
+
+
+        } else {
+
+        if (description.includes('clear')) {
+            backgroundImage = "url('https://images.pexels.com/photos/813269/pexels-photo-813269.jpeg?auto=compress&cs=tinysrgb&w=600')";
+        } else if (description.includes('clouds')) {
+            backgroundImage = "url('https://images.unsplash.com/photo-1572162522099-7a0c28d7691b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+        } else if (description.includes('rain')) {
+            backgroundImage = "url('https://images.pexels.com/photos/2068411/pexels-photo-2068411.jpeg?auto=compress&cs=tinysrgb&w=600')";
+        } else if (description.includes('snow')) {
+            backgroundImage = "url('https://images.unsplash.com/photo-1542601098-8fc114e148e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+        } else if (description.includes('thunderstorm')) {
+            backgroundImage = "url('https://images.pexels.com/photos/3657143/pexels-photo-3657143.jpeg?auto=compress&cs=tinysrgb&w=600')";
+        } else {
+            backgroundImage = "url('https://static.vecteezy.com/system/resources/previews/035/987/337/large_2x/ai-generated-clear-night-sky-filled-with-stars-that-seem-to-twinkle-against-a-dark-backdrop-free-photo.jpg')"; // Fallback image
+        }
+
+        
+        }
+        // Set the background image
+        body.style.backgroundImage = backgroundImage;
         
         showImage()
 
